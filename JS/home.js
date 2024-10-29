2ab3f1eb1ad34e480c314be135a839fa4e43a0ad
Rafee <rafeehasan88@gmail.com>
2024-10-29 11:02:46 +0600

    cash out calculation added

--- JS/home.js
+++ JS/home.js
@@ -1,4 +1,11 @@
-// add money to the account
+/**
+ * input field hle value diye access korte hy
+ * onno kisu hle innerText use kora lage
+ */
+
+
+
+    /**----------------------------------------Add Money....................................................... */
 
 
 // step-1: add event handler to the add money button inside the form
@@ -11,7 +18,78 @@ document.getElementById('btn-add-money').addEventListener('click' , function(eve
     
 
     // get the pin number
-    const pinNumber = document.getElementById('input-pin').value;
+    const pinNumberInput = document.getElementById('input-pin').value;
+
+    // verify pin number(wrong way)
+    if(pinNumberInput === '1234'){
+        
+
+        // get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        
+        
+        // add addMoneyInput and balance
+        const addMoneyAmount = parseFloat(addMoneyInput);
+        const balanceAmount = parseFloat(balance);
+
+        const updatedBalance = addMoneyAmount + balanceAmount;
     
 
-})
+        // update the balance in the ui/dom
+
+        document.getElementById('account-balance').innerText = updatedBalance;
+
+
+
+    }
+    else{
+        alert('Failed! Please try again!');
+    }
+});
+
+
+
+    /**----------------------------------------Cash out money....................................................... */
+
+// step-1: add event handler to the cash out button inside the form
+document.getElementById('btn-cash-out').addEventListener('click' , function(event){
+
+    // prevent page reload after form submit
+    event.preventDefault();
+
+    // get Cashout amount 
+    const cashoutInput = document.getElementById('input-cashout').value;
+
+    // get pin number
+    const inputPinNumber = document.getElementById('input-cashout-pin').value;
+
+    // verify pin number(wrong way)
+    if(inputPinNumber === '1234'){
+
+        // get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+
+         // reduce operation between addMoneyInput and balance
+         const inputAmount = parseFloat(cashoutInput);
+         const balanceAmountNumber = parseFloat(balance);
+
+         const remainingAmount = balanceAmountNumber - inputAmount;
+
+        //  check if input amount is valid and less than or equal to balance
+
+         if (isNaN(inputAmount) || inputAmount <= 0 || inputAmount > balanceAmountNumber) {
+            alert("Invalid cashout amount!");
+            return;
+        }
+
+         // reduce the balance in the ui/dom
+         document.getElementById('account-balance').innerText = remainingAmount;
+         
+        
+    }
+    else{
+        alert("Sorry! Try again!");
+    } 
+
+
+});
